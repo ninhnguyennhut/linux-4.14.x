aac482e2b4266d54115e74f49983e878d0e2a851
ninhnguyennhut <nguyennhutninh@live.com>
2018-12-08 21:38:06 +0700

    add all data

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP Sat Dec 8 19:10:43 +07 2018"
+#define LINUX_COMPILE_BY "fanning"
+#define LINUX_COMPILE_HOST "Lotus"
+#define LINUX_COMPILER "gcc version 7.3.0 (GCC)"
