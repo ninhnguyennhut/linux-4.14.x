ff016693b8be8166c4817f2830bf97f3acae6f1b
nguyennhutninh <nguyennhutninh@live.com>
2019-04-08 16:29:08 +0700

    add support keyboard

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 3 */
-/* SMP */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#3 SMP Sat Feb 23 09:00:45 +07 2019"
-#define LINUX_COMPILE_BY "fanning"
-#define LINUX_COMPILE_HOST "Lotus"
-#define LINUX_COMPILER "gcc version 7.2.1 20171011 (Linaro GCC 7.2-2017.11)"
--- include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "4.13.0-rc7-licheepi-zero+"
